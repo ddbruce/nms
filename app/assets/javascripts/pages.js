adefc82ef84a94c9aa562763b48f73a2924a1ca4
Dan Bruce <daniel@danieldbruce.com>
2014-07-09 15:56:22 -0400

    added looping for main index articles

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,11 +1,12 @@
-function run() {
-	$('#news').transition({opacity: 1},1000,'in-out').delay(5000).transition({opacity: 0},1000,'in-out', function() {
-		$('#features').transition({opacity: 1},1000,'in-out').delay(5000).transition({opacity: 0},1000,'in-out', run);
+function run(id) {
+	$(id)
+	.transition({opacity: 1},1000,'in-out')
+	.delay(5000)
+	.transition({opacity: 0},1000,'in-out', function() {
+		run(this.data('next'))
 	});
 }
 
-/*use data tag in html5 to call function*/ /*use function() {run(nextdivhere)};*/
-
 $( document ).ready(function() {
-	run();
+	run('#news');
 });
